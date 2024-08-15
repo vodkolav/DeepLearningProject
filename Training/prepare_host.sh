a6a2d070c11717b42dadb0316017c80a663afd0f
Michael Berger <vo1kod4v@gmail.com>
2024-08-15 15:45:04 +0300

    Small fix to scripts

--- Training/prepare_host.sh
+++ Training/prepare_host.sh
@@ -23,7 +23,6 @@ sudo apt-get update
 ## Install the Docker packages. 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-
 ## Verify that the Docker Engine installation is successful by running the hello-world image.
 sudo docker run hello-world
 
